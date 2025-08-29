61fd0148ebaf774e8a5e0f87fbdd0271b7df51ca
Mateusz Olszewski <zeeevq1@gmail.com>
2025-08-29 14:32:51 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import EventDetail from "./pages/EventDetail";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import CreateEvent from "./pages/CreateEvent";
+
 function App() {
   return (
-    <>
-      <h1>Hi</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events/:id" element={<EventDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/create" element={<CreateEvent />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
